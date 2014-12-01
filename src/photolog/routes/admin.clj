aa276c7f7e47e56c1f082df53f514c01c20347b7
sirMackk <c.tairix@gmail.com>
2014-12-01 07:17:03 -0500

    Adds saving thumbnail to album directory

--- src/photolog/routes/admin.clj
+++ src/photolog/routes/admin.clj
@@ -18,6 +18,10 @@
             
 
 ; these can go live in like album utils yo
+
+(defn slugidize [string]
+  (st/replace (st/trim (st/lower-case string)) #"[^a-z0-9-]+" "-"))
+
 (defn scale [img ratio width height]
   "Creates an AffineTransform Scale object using the ration, then, feeds that
   to the AffineTransfromOp constructor. Finally, the filter method of transform-op
@@ -35,18 +39,15 @@
         ratio 0.5]
     (scale img ratio (int (* img-width ratio)) (int (* img-height ratio)))))
 
-(defn save-thumbnail [{:keys [filename]}]
+(defn save-thumbnail [{:keys [filename]} album-name]
   "Creates an empty file (File.) and uses ImageIO/write
   to write BufferedImage into that file."
-  (let [path (str albums File/separator)]
+  (let [path (str albums File/separator (slugidize album-name) File/separator)]
     (with-open [output_image (io/input-stream (str path filename))]
       (ImageIO/write
         (scale-image output_image)
         "jpeg"
         (File. (str path thumb-prefix filename))))))
-
-(defn slugidize [string]
-  (st/replace (st/lower-case string) #"[^a-z0-9-]+" "-"))
 
 (defn admin-index [r]
   (layout/admin
@@ -77,8 +78,8 @@
     (let [new_album (db/insert-album form (session/get :user))]
       (doseq [photo photos]
         (upload-file (str albums File/separator (slugidize (get form :name))) photo :create-path? true)
-        (db/insert-photo-into-album photo (get new_album :id))))
-        ;thumbnail
+        (db/insert-photo-into-album photo (get new_album :id))
+        (save-thumbnail photo (get form :name))))
     (catch Exception ex
       (str "Error uploading file: " (.getMessage ex)))))
 
